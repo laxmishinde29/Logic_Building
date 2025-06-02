a02ffd062d3195bd3bad19ffa0a96bd6cca73732
laxmishinde29 <laxmishinde2975@gmail.com>
2025-06-02 21:41:09 +0530

    Add files via upload

---
+++ LB_Assignment/Assi16_2.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+    char ch1 = '\0';
+
+    //    1       2        3
+    for(i = 1; i<= iRow; i++)   // Outer
+    {   //    1         2       3 
+        for(j = 1,ch1 = 'a'; j <= iCol; j++,ch1++)  // Inner
+        {
+            if( i == j)
+            {
+                printf("*\t");     
+            }
+            else if( i > j)
+            {
+                printf("*\t");
+            }
+            else
+            {
+                printf("#\t");
+            }
+        }
+        
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows : \n");
+    scanf("%d",&iValue1);
+
+    printf("Enter number of columns : \n");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
